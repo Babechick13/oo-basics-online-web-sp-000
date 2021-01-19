e8b58a0292e945256d9f41454fa31dedfdac84ad
Jessica Natovich <jessicanatovich@yahoo.com>
2021-01-19 02:09:44 +0000

    Automatically backed up by Learn

--- spec/02_shoe_spec.rb
+++ spec/02_shoe_spec.rb
@@ -22,3 +22,11 @@ describe 'Shoe' do
     it 'says that the shoe has been repaired' do
       expect($stdout).to receive(:puts).with("Your shoe is as good as new!")
       shoe.cobble
+    end
+    it 'makes the shoe\'s condition new' do
+      shoe.condition = "old"
+      shoe.cobble
+      expect(shoe.condition).to eq("new")
+    end
+  end
+end
